50435d9441c00c6b9212e3f14e52157ae72d6299
Rene - Desktop <renemv@gmail.com>
2024-09-14 23:12:19 -0700

    Updated for exception if file is unable to be moved.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 """
 import argparse
 import os
+import shutil
 import datetime
 import pathlib
 import glob
@@ -30,36 +31,48 @@ def find_images(directory, image_ext='jpg'):
 
 
 def rename_image(image_file, storage_directory='.', dryrun=False):
-    """ Extracts the metadata for the given image and puts the file in a folder based on year-month."""
-    # Store the file name
-    file_name = os.path.basename(image_file)
-    # Read the image data using PIL
-    oimage = Image.open(image_file)
-    # Extract exif data
-    exifdata = oimage.getexif()
-    for tag_id in exifdata:
-        # Get the tag name
-        tag = TAGS.get(tag_id, tag_id)
-        data = exifdata.get(tag_id)
-        # Only use the datetime tag
-        if tag.lower() == 'datetime':
-            parsed_date = datetime.datetime.strptime(data, '%Y:%m:%d %H:%M:%S')
-            month = parsed_date.strftime('%B')
-            place_to_store = os.path.join(storage_directory, f'{parsed_date.year}', f'{month}', file_name)
+    """ Extracts the metadata for the given image and puts the file in a folder based on year-month.
 
-            print(f'Moving {image_file} to {place_to_store}')
-            if dryrun:
-                continue
-            # Move image to new location
-
-
-if __name__ == "__main__":
-    """
     https://stackoverflow.com/questions/237079/how-do-i-get-file-creation-and-modification-date-times
     fname = pathlib.Path('test.py')
     ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime, tz=datetime.timezone.utc)
     mtime = datetime.datetime.fromtimestamp(fname.stat().st_mtime, tz=datetime.timezone.utc)
     """
+    # Store the file name
+    file_name = os.path.basename(image_file)
+    # Read the image data using PIL
+    oimage = Image.open(image_file)
+    fname = pathlib.Path(image_file)
+    ctime = datetime.datetime.fromtimestamp(fname.stat().st_ctime, tz=datetime.timezone.utc)
+    mtime = datetime.datetime.fromtimestamp(fname.stat().st_mtime, tz=datetime.timezone.utc)
+    earliest_date = min(ctime, mtime)
+    # Extract exif data
+    exifdata = oimage.getexif()
+    if exifdata:
+        for tag_id in exifdata:
+            # Get the tag name
+            tag = TAGS.get(tag_id, tag_id)
+            data = exifdata.get(tag_id)
+            # Only use the datetime tag
+            if tag.lower() == 'datetime':
+                parsed_date = datetime.datetime.strptime(data, '%Y:%m:%d %H:%M:%S')
+                earliest_date = min(earliest_date, parsed_date)
+    month = earliest_date.strftime('%B')
+    place_to_store = os.path.join(storage_directory, f'{earliest_date.year}', f'{month}', file_name)
+
+    print(f'Moving {image_file} to {place_to_store}')
+    if dryrun:
+        return
+    os.makedirs(os.path.dirname(place_to_store), exist_ok=True)
+    try:
+        shutil.move(image_file, place_to_store)
+    except PermissionError as e:
+        print(f'Could not move {image_file} to {place_to_store}: {e}')
+        return
+
+
+if __name__ == "__main__":
+
     # Parse user supplied information
     parser = argparse.ArgumentParser(prog='Picture Organizer',
                                      description='Organizes images into separate directories.')
